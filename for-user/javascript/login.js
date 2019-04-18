55f696ccc965a1936914c1ed8c17b886a35f6b0d
ctoc_takaaki <takaaki.nc.jpn@gmail.com>
2019-04-18 18:53:34 +0900

    for-company

--- for-user/javascript/login.js
+++ for-user/javascript/login.js
@@ -2,10 +2,17 @@ $(function() {
     $('#login-show').click(function() {
       $('#login-modal').fadeIn();
     });
+    $('#login-show2').click(function() {
+      $('#login-modal2').fadeIn();
+    });
     $('.close-modal').click(function() {
         $('#login-modal').fadeOut();
         $('#signup-modal').fadeOut();
       });
+    $('.close-modal2').click(function() {
+      $('#login-modal2').fadeOut();
+      $('#signup-modal2').fadeOut();
+    });
   });
 
 
