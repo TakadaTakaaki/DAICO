21d2cc73a66ba9aa280625b58e6ffb8f7a56a721
TakadaTakaaki <takaaki.nc.jpn@gmail.com>
2019-05-06 02:23:51 +0900

    userのURL/名称設定

--- daico_engine/engine/urls.py
+++ daico_engine/engine/urls.py
@@ -2,8 +2,50 @@ from django.conf.urls import url
 from . import views
  
 urlpatterns = [
+    # user
     url(r'^$', views.index, name='index'),
+    url(r'article', views.article, name="article"),
+    url(r'article_detail', views.article_detail, name="article_detail"),
+    url(r'company', views.company, name="company"),
+    url(r'blog', views.blog, name="blog"),
+    url(r'blog_detail', views.blog_detail, name="blog_detail"),
+    url(r'plan', views.plan, name="plan"),
+    url(r'review', views.review, name="review"),
+    url(r'staff', views.staff, name="staff"),
+    url(r'staff_detail', views.staff_detail, name="staff_detail"),
+    url(r'chat', views.chat, name="chat"),
     url(r'favorite', views.favorite, name="favorite"),
-    url(r'store', views.store, name="store"),
+    url(r'login', views.login, name="login"),
+    url(r'notice_company', views.notice_company, name="notice_company"),
+    url(r'notice_company_detail', views.notice_company_detail, name="notice_company_detail"),
+    url(r'notice_engine', views.notice_engine, name="notice_engine"),
+    url(r'notice_engine_detail', views.notice_engine_detail, name="notice_engine_detail"),
+    url(r'order', views.order, name="order"),
+    url(r'order_detail', views.order_detail, name="order_detail"),
+    url(r'order_cancel', views.order_cancel, name="order_cancel"),
+    url(r'order_cancel_detail', views.order_cancel_detail, name="order_cancel_detail"),
+    url(r'order_notExecuted', views.order_notExecuted, name="order_notExecuted"),
+    url(r'order_notExecuted_detail', views.order_notExecuted_detail, name="order_notExecuted_detail"),
+    url(r'register', views.register, name="register"),
+    url(r'register_detail', views.register_detail, name="register_detail"),
+    url(r'reserve', views.reserve, name="reserve"),
+    url(r'reserve_date', views.reserve_date, name="reserve_date"),
+    url(r'reserve_date_detail', views.reserve_date_detail, name="reserve_date_detail"),
+    url(r'reserve_payment', views.reserve_payment, name="reserve_payment"),
+    url(r'reserve_payment_detail', views.reserve_payment_detail, name="reserve_payment_detail"),
+    url(r'reserve_done', views.reserve_done, name="reserve_done"),
+    url(r'search', views.search, name="search"),
+    url(r'setting_companyInquiry', views.setting_companyInquiry, name="setting_companyInquiry"),
+    url(r'setting_contact', views.setting_contact, name="setting_contact"),
+    url(r'setting_credit', views.setting_credit, name="setting_credit"),
+    url(r'setting_creditAdd', views.setting_creditAdd, name="setting_creditAdd"),
+    url(r'setting_creditEdit', views.setting_creditEdit, name="setting_creditEdit"),
+    url(r'setting_hidden', views.setting_hidden, name="setting_hidden"),
+    url(r'setting_tpoint', views.setting_tpoint, name="setting_tpoint"),
+    url(r'setting_unsubscribe', views.setting_unsubscribe, name="setting_unsubscribe"),
+    url(r'setting_userDetailChange', views.setting_userDetailChange, name="setting_userDetailChange"),
+    
+    # company
+    # engine
 
 ]
--- daico_engine/engine/views.py
+++ daico_engine/engine/views.py
@@ -4,10 +4,107 @@ from django.shortcuts import render
 
 from django.http.response import HttpResponse
 from django.shortcuts import render
- 
+
+# user
 def index(request):
     return render(request, 'user/home/home.html')
+
+# 基本_detail系はあとで_uuid作成してリンクさせるはず
+
+# article 記事
+def article(request):
+    return render(request, 'user/article/article.html')
+def article_detail(request):
+    return render(request, 'user/article/article_detail.html')
+# company 会社詳細
+def company(request):
+    return render(request, 'user/company/company.html')
+def blog(request):
+    return render(request, 'user/company/blog/blog.html')
+def blog_detail(request):
+    return render(request, 'user/company/blog/blog_detail.html')
+def plan(request):
+    return render(request, 'user/company/plan/plan.html')
+    # 裏側見てから決めるページを分けるのか表示方法でどうにかするのか
+def review(request):
+    return render(request, 'user/company/review/review.html')
+def staff(request):
+    return render(request, 'user/company/staff/staff.html')
+def staff_detail(request):
+    return render(request, 'user/company/staff/staff_detail.html')
+# chat お問い合わせ user⇔company
+def chat(request):
+    return render(request, 'user/chat/chat.html')
+# favorite お気に入り
 def favorite(request):
     return render(request, 'user/favorite/favorite.html')
-def store(request):
-    return render(request, 'user/notification/store.html')
+# login ログイン画面
+def login(request):
+    return render(request, 'user/login/login.html')
+# notice 通知
+def notice_company(request):
+    return render(request, 'user/notice/company/company.html')
+def notice_company_detail(request):
+    return render(request, 'user/notice/company/company_detail.html')
+def notice_engine(request):
+    return render(request, 'user/notice/engine/engine.html')
+def notice_engine_detail(request):
+    return render(request, 'user/notice/engine/engine_detail.html')
+# order 注文履歴
+def order(request):
+    return render(request, 'user/order/order.html')
+def order_detail(request):
+    return render(request, 'user/order/order_detail.html')
+def order_cancel(request):
+    return render(request, 'user/order/cancel/cancel.html')
+def order_cancel_detail(request):
+    return render(request, 'user/order/cancel/cancel_detail.html')
+def order_notExecuted(request):
+    return render(request, 'user/order/notExecuted/notExecuted.html')
+def order_notExecuted_detail(request):
+    return render(request, 'user/order/notExecuted/notExecuted_detail.html')
+# register 新規登録
+def register(request):
+    return render(request, 'user/register/register.html')
+def register_detail(request):
+    return render(request, 'user/register/register_detail.html')
+# reserve 予約
+def reserve(request):
+    return render(request, 'user/reserve/reserve.html')
+def reserve_date(request):
+    return render(request, 'user/reserve/date/date.html')
+def reserve_date_detail(request):
+    return render(request, 'user/reserve/date/date_detail.html')
+def reserve_payment(request):
+    return render(request, 'user/reserve/payment/payment.html')
+def reserve_payment_detail(request):
+    return render(request, 'user/reserve/payment/payment_detail.html')
+def reserve_done(request):
+    return render(request, 'user/reserve/done/done.html')
+# search 検索
+def search(request):
+    return render(request, 'user/search/search.html')
+# setting 設定
+def setting_companyInquiry(request):
+    return render(request, 'user/setting/companyInquiry/companyInquiry.html')
+def setting_contact(request):
+    return render(request, 'user/setting/contact/contact.html')
+# creditについては変更と追加ができないとだめだと思います
+def setting_credit(request):
+    return render(request, 'user/setting/credit/credit.html')
+def setting_creditAdd(request):
+    return render(request, 'user/setting/credit/creditAdd.html')
+def setting_creditEdit(request):
+    return render(request, 'user/setting/credit/creditEdit.html')
+def setting_hidden(request):
+    return render(request, 'user/setting/hidden/hidden.html')
+def setting_tpoint(request):
+    return render(request, 'user/setting/tpoint/tpoint.html')
+def setting_unsubscribe(request):
+    return render(request, 'user/setting/unsubscribe/unsubscribe.html')
+def setting_userDetailChange(request):
+    return render(request, 'user/setting/userDetailChange/userDetailChange.html')
+
+
+# company
+# engine
