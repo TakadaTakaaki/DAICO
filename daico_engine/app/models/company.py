0ec0296afb405ade7a8de197a2df8a02bc2ddb0f
ctoc_mitsuki <ma.090109.1997@gmail.com>
2019-05-25 22:34:12 +0900

    a

---
+++ daico_engine/app/models/company.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+
+
+
+class Company_data(models.Model):
+    class Meta:
+        db_table = 'company_data'
+
+    name = models.CharField(max_length=30)
+    pr = models.CharField(max_length=70)
+    tell = models.CharField(max_length=12)
+    price = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    year = models.CharField(max_length=10)
+    credit = models.CharField(max_length=50)
+    homepage = models.URLField(max_length=100)
+    staff = models.CharField(max_length=10)
+    time = models.CharField(max_length=20)
+    service = models.TextField()
+    pay = models.TextField()
+    cancel = models.TextField() 
+    security = models.TextField()
+
+    def __str__(self):
+        return '<id:' + str(self.id) + ',' + self.name + ',' + self.staff + '>'
+
+class Staff(models.Model):
+    class Meta:
+        db_table = 'staff'
+
+    company_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    position = models.CharField(max_length=10)
+    description = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.company_name
+
+class Plan(models.Model):
+    class Meta:
+        db_table = 'plan'
+
+    company_plan = models.CharField(max_length=30)
+    genre = models.CharField(max_length=8)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=75)
+    price = models.CharField(max_length=30)
+    terms = models.CharField(max_length=20)
+    effect = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.company_plan
