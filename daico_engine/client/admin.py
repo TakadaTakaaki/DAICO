d61bddb7b02fb1bad4f235e397bd2cf03776a92a
ctoc_mitsuki <ma.090109.1997@gmail.com>
2019-05-26 18:15:14 +0900

    a

--- daico_engine/client/admin.py
+++ daico_engine/client/admin.py
@@ -1,3 +1,13 @@
 # from django.contrib import admin
 # from .models import post
 from django.contrib import admin
+# from .models import Company_data
+# from .models import Staff
+# from .models import Plan
+
+# admin.site.register(Company_data)
+
+# admin.site.register(Staff)
+
+# admin.site.register(Plan)
+
